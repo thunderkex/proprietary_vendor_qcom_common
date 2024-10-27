fe7102820f866786db3fe4e160f9ba1f1e9ddca5
Omkar Chandorkar <gotenksIN@aospa.co>
2024-10-27 09:10:16 +0530

    system: Enable ELF checks
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- system/bt/bt-vendor.mk
+++ system/bt/bt-vendor.mk
@@ -5,6 +5,6 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/common/system/bt
 
-PRODUCT_COPY_FILES += \
-    vendor/qcom/common/system/bt/proprietary/system_ext/lib/com.qualcomm.qti.ant@1.0.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib/com.qualcomm.qti.ant@1.0.so \
-    vendor/qcom/common/system/bt/proprietary/system_ext/lib64/com.qualcomm.qti.ant@1.0.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/com.qualcomm.qti.ant@1.0.so
+
+PRODUCT_PACKAGES += \
+    com.qualcomm.qti.ant@1.0
